29af88e279d96b825bf75faad8ea3a0fb20a7057
TianhuaLu <tluu0022@student.monash.edu>
2025-03-30 13:30:39 +1100

    EShop Microservices - Catalog microservices using CQRS pattern (Marten, MediatR, Mapster, Carter)

---
+++ eshop-microservices/srs/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.GetProductByCategory
+{
+	public record GetProductsByCategoryQuery(string category) : IQuery<GetProductsByCategoryResult>;
+
+	public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+	internal class CreateProductsByCategoryQueryHandler(IDocumentSession session, ILogger<CreateProductsByCategoryQueryHandler> logger) 
+		: IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+	{
+		public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+		{
+			logger.LogInformation("GetProductsByCategoryQueryHandler.Handle called with {@Query}", query);
+
+			var products = await session.Query<Product>()
+				.Where(p => p.Category.Contains(query.category))
+				.ToListAsync();
+
+			return new GetProductsByCategoryResult(products);
+		}
+	}
+}
